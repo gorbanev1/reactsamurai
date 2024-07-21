4b6f4768b80293b43974ca623e1af6f6a325531a
gorban <gorbanev_i@mail.ru>
2024-07-21 13:21:44 +0300

    76-10

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,48 +4,55 @@ import {Navigate, NavLink} from "react-router-dom";
 import DialogItem from "./DialogItem/DialogsItem";
 import Message from "./Message/Message";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/store";
+import {Field, reduxForm} from "redux-form";
 
 const Dialogs = (props) => {
-    let state=props.dialogsPage
+    let state = props.dialogsPage
     const dialogsElements = state.dialogsData.map(d => <DialogItem name={d.name} id={d.id} key={d.id}/>)
     const messagesElements = state.messagesData.map(m => <Message message={m.message} id={m.id} key={m.id}/>)
     const newMessageBody = state.newMessageBody
 
-    let newMessageElement=React.createRef()
+    let newMessageElement = React.createRef()
 
-    const onSendMessageClick=()=>{
+    const onSendMessageClick = () => {
         props.sendMessage()
-        let text=newMessageElement.current.value;
+        let text = newMessageElement.current.value;
 
 
         /*alert(text)*/
     }
-    const onNewMessageChange=(e)=>{
+    const onNewMessageChange = (e) => {
         let body = e.target.value
         debugger
         props.updateNewMessageBody(body)
-        //props.dispatch(updateNewMessageBodyCreator(body))
-
-
     }
-    if(!props.isAuth) return <Navigate to={"/login"}/>
-        return (<div className={css.dialogs}>
+    const addNewMessage = (values) => {
+        alert(values.newMessageBody);
+    }
+    if (!props.isAuth) return <Navigate to={"/login"}/>
+    return (<div className={css.dialogs}>
             <div className={css.dialogsItems}>
                 {dialogsElements}
 
             </div>
             <div className={css.messages}>
                 <div>{messagesElements}</div>
-
-                <div>
-                    <textarea ref={newMessageElement}
-                              onChange={onNewMessageChange}
-                              value={newMessageBody}/></div>
-                <div>
-                    <button onClick={onSendMessageClick}> Add blya</button>
-                </div>
+                <AddMessageFormRedux onSubmit={addNewMessage}/>
             </div>
         </div>
     )
 }
+const AddMessageForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component="textarea" name={"newMessageBody"} placeholder="Enter your mesage"/>
+            </div>
+            <div>
+                <button> Add blya</button>
+            </div>
+        </form>
+    )
+}
+const AddMessageFormRedux=reduxForm({form: "dialogAddMessageForm"})(AddMessageForm);
 export default Dialogs
