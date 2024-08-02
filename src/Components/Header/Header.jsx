19e5be425fc4fc3dc63dc55329f56f59fbebdfb6
Igor Gorbanev <gorbanev_i@mail.ru>
2024-08-02 17:51:04 +0300

    78

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
 
         <img src='https://sun9-64.userapi.com/impg/IRFYJFgpFK8yx8A26iDUKsPcFHnBZRBJdSEang/Q9xQnu4uGCk.jpg?size=1539x1539&quality=95&sign=43741dcb064b6bc41f587d581fafeef2&type=album' />
         <div className={cssheader.loginBlock}>
-        {props.isAuth?props.login
+        {props.isAuth?<div>{props.login}<button onClick={props.logout}>Log out</button></div>
             : <NavLink to={'/login'}>Login</NavLink>}
     </div>
     </header>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {Navigate} from "react-router-dom";
 
 const LoginForm = (props) => {
     return <form onSubmit={props.handleSubmit}>
@@ -11,7 +14,7 @@ const LoginForm = (props) => {
                    component={Input}/>
         </div>
         <div>
-            <Field placeholder={"Password"} name={"password"}
+            <Field placeholder={"Password"} name={"password"} type={"password"}
                    validate={[required]}
                    component={Input}/>
         </div>
@@ -30,12 +33,18 @@ const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
     const onSubmit=(formData)=>{
-        console.log(formData)
+        props.login(formData.email, formData.password, formData.rememberMe)
 
+    }
+    if (props.isAuth){
+        return  <Navigate to={"/profile"}/>
     }
     return <div>
         <h1>Login</h1>
         <LoginReduxForm onSubmit={onSubmit}/>
     </div>
 }
-export default Login
+const mapStateToProps=(state)=>({
+    isAuth: state.auth.isAuth
+})
+export default connect(mapStateToProps,{login})(Login)
